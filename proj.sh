305bb25135cad703861b6a89e0a30ee54bfedc57
Rodrigo Mibielli <rodrigo.mibielli@PC-103.impa.br>
2019-12-03 20:05:41 -0300

    Created Spring Cloud Server Config Archetype.

---
+++ proj.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mvn archetype:generate  -DarchetypeGroupId=com.rodmibielli.archetypes -DarchetypeArtifactId=maven-spring-cloud-server-config-archetype -DarchetypeVersion=1.0.0-SNAPSHOT
+

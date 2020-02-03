1e6c9f757f3bce9eb7d15e6aff7f22bfac0c5846
Rodrigo Mibielli <rodrigo.mibielli@PC-103.impa.br>
2020-02-03 16:56:11 -0300

    CHG: Added configuration for basic realm.

--- src/main/resources/archetype-resources/src/main/java/ConfigServerApplication.java
+++ src/main/resources/archetype-resources/src/main/java/ConfigServerApplication.java
@@ -1,8 +1,13 @@
 package ${package};
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * This is the main entry point of Spring Cloud Config Server.
@@ -16,4 +21,18 @@ public class ConfigServerApplication {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
+	@Autowired
+    @Bean
+    public WebSecurityConfigurerAdapter configurationServerWebSecurity(final ServerProperties serverProperties) {
+        return new WebSecurityConfigurerAdapter() {
+        	
+        	 @Override
+        	    protected void configure(HttpSecurity http) throws Exception {
+        	        http.csrf().disable().authorizeRequests()
+        	        .anyRequest().authenticated().and()
+        	        .httpBasic();
+        	    }
+        };
+	}
+	
 }
